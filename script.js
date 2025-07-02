4399d43fb4d4ffa7bf8a73bf26a9b0b7f033fcb8
Joseph Mensah <alvinjoemens@gmail.com>
2025-07-02 20:57:28 +0000

    Added new features

---
+++ script.js
@@ -0,0 +1,8 @@
+const stars = document.querySelectorAll('.rating input');
+stars.forEach(star => {
+    star.addEventListener('change', () => {
+        const ratingValue = star.id.replace('star', '');
+        alert(`You rated ${ratingValue} star${ratingValue > 1 ? 's' : ''}`);
+    });
+});
+27  
