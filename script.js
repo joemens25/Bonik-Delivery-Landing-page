ce0dde3a257e41194dc4885362016e2371010fa9
Joseph Mensah <alvinjoemens@gmail.com>
2025-07-10 13:06:26 +0000

    Changes made

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const stars = document.querySelectorAll('.rating input');
 stars.forEach(star => {
     star.addEventListener('change', () => {
         const ratingValue = star.id.replace('star', '');
-        alert(`You rated ${ratingValue} star${ratingValue > 1 ? 's' : ''}`);
+        alert(`You rated ${ratingValue} star${ratingValue > 1 ? 's' : ''}`);
     });
 });
-27  
